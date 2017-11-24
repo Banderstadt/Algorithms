49b993411f23e568d6e04f8d282932f7bfc98453
Vasyl Zadorozhnyy <amdpaxton@gmail.com>
2017-11-24 11:27:45 +0200

    Quick Sort algorithm has been added

--- algorithms.js
+++ algorithms.js
@@ -1,13 +1,13 @@
 // Quick Sort algorithm
 function quickSort(arr) {
-	if (arr.length <= 1) return arr;
-	var pivot = arr[arr.length - 1],
-	    less = [],
-		more = [];
-	for (var i = 0; i < arr.length - 1; i++) {
-		if (arr[i] < pivot) {
-			less.push(arr[i])
-		} else more.push(arr[i])
-	}
-	return [...quickSort(less), pivot, ...quickSort(more)];
+    if (arr.length <= 1) return arr;
+    var pivot = arr[arr.length - 1],
+        less = [],
+        more = [];
+    for (var i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            less.push(arr[i])
+        } else more.push(arr[i])
+    }
+    return [...quickSort(less), pivot, ...quickSort(more)];
 }
